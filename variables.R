d743891aab6daa1b4bc2e63192d543c30d7fc492
Afshan2928 <145640851+Afshan2928@users.noreply.github.com>
2024-01-18 11:59:43 +0530

    Add files via upload

---
+++ variables.R
@@ -0,0 +1,35 @@
+a<-8
+a
+strg1<-"hello R"
+strg1
+#Paste()
+srtring1<-"awesome"
+paste("look",srtring1)
+text1<-"R is"
+text2<-"awesome"
+paste(text1,text2)
+num1<-6
+num2<-7
+num1+num2
+
+
+num<-3
+str="hello maya"
+num+str
+
+#multiple variables assignments
+a <- b<- c<-"orange"
+a
+b
+c
+
+var1 <- var2 <- var3 <-"chocolate"
+var1
+var2
+var3
+
+male<-female<-"gender"
+male
+female
+
+
